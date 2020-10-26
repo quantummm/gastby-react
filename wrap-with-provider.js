4fe561f3f7ce1e2269fc013b056caf505660aa6a
Rongzhi Wang <aidenwang0826@gmail.com>
2020-10-26 16:42:37 +1100

    add redux deve tool

--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './src/store/reducer';
 import createSagaMiddleware from 'redux-saga';
 import { mySaga } from './src/store/sagas/index';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+const composeEnhancers =
+  (typeof window !== 'undefined' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware));
+
+const store = createStore(reducer, enhancer);
 
 sagaMiddleware.run(mySaga);
 
